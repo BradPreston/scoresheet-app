95384061b6a7d9c79cd362f647fcbf27613f9a63
= <=>
2025-07-04 07:12:43 -0400

    Replace the content in index.vue with a simple Hero

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <Hero>
+      <template v-slot:heading>Game night just got easier</template>
+      <template v-slot:subheading
+        >You handle the fun. We'll handle the math.</template
+      >
+      <template v-slot:action>
+        <UButton :onclick="startNewGame">Start New Game</UButton>
+      </template>
+    </Hero>
   </div>
 </template>
+
+<script setup lang="ts">
+const startNewGame = () => navigateTo("/new-game");
+</script>
