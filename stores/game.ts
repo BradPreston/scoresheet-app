c7f0b2284123258f7017cd34e3a9555393c94f35
bradpreston <bap5393@gmail.com>
2025-07-04 07:45:50 -0400

    Add start and reset functions to game store

--- stores/game.ts
+++ stores/game.ts
@@ -1,7 +1,9 @@
-import { defineStore } from "pinia";
+import { defineStore } from 'pinia';
 
-export const gameStore = defineStore("game", () => {
-  const started = ref(false);
+export const useGameStore = defineStore('game', () => {
+	const started = ref(false);
+	const start = () => (started.value = true);
+	const reset = () => (started.value = false);
 
-  return { started };
+	return { started, start, reset };
 });
