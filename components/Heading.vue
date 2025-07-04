2345a25f99001a9cae0ae2bc4cb8395befac793d
= <=>
2025-07-04 07:11:13 -0400

    Created a simple Heading, Hero, and Paragraph component

---
+++ components/Heading.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1 v-if="type == 'h1'" class="text-5xl font-black"><slot /></h1>
+  <h2 v-if="type == 'h2'" class="text-3xl font-extrabold"><slot /></h2>
+  <h3 v-if="type == 'h3'" class="text-xl font-bold"><slot /></h3>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  type: "h1" | "h2" | "h3";
+}>();
+</script>
---
+++ components/Hero.vue
@@ -0,0 +1,7 @@
+<template>
+  <section class="mx-auto text-center py-12">
+    <Heading type="h1"><slot name="heading" /></Heading>
+    <Paragraph class="mt-2 mb-4 text-xl"><slot name="subheading" /></Paragraph>
+    <slot name="action" />
+  </section>
+</template>
