25d4112cbd8d4c6252eeca888c3fd2cc7192f0c9
= <=>
2025-07-04 07:13:34 -0400

    Create simple new-game and scoresheet pages

---
+++ pages/new-game.vue
@@ -0,0 +1,16 @@
+<template>
+  <Heading type="h1">New Game</Heading>
+  <UButton :onclick="startGame">Start</UButton>
+</template>
+
+<script setup lang="ts">
+import { gameStore } from "#imports";
+const game = gameStore();
+const { started } = storeToRefs(game);
+
+const startGame = async () => {
+  console.log("clicked");
+  started.value = true;
+  await navigateTo("/scoresheet");
+};
+</script>
---
+++ pages/scoresheet.vue
@@ -0,0 +1,20 @@
+<template>
+  <Heading type="h1">Scoresheet</Heading>
+  <UButton :onclick="reset">Start Over</UButton>
+</template>
+
+<script setup lang="ts">
+import { gameStore } from "#imports";
+
+const game = gameStore();
+const { started } = storeToRefs(game);
+
+const reset = async () => {
+  started.value = false;
+  await navigateTo("/new-game");
+};
+
+if (!game.started) {
+  await navigateTo("/new-game");
+}
+</script>
