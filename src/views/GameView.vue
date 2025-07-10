63491ba377d21c7f6a33a3a6465a9d8dbf502f51
bradpreston <bap5393@gmail.com>
2025-07-10 18:17:06 -0400

    Add simple GameView with game started checker

---
+++ src/views/GameView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { useGameStore } from '@/stores/game';
+import { useRouter } from 'vue-router';
+const router = useRouter();
+const gameStore = useGameStore();
+if (!gameStore.started) {
+  router.push({ name: 'home' });
+}
+</script>
+
+<template>
+  <main>
+    <h1>Game page</h1>
+  </main>
+</template>
